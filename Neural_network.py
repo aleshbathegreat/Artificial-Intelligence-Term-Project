7b79a11b38facf109c614bb34d4bf30068daf1dc
bareerah16 <64607538+bareerah16@users.noreply.github.com>
2025-05-04 21:51:08 +0500

    updated Neural_network.py
    The average valence and arousal data from annotations per song, from their CSV files, were combined to calculate the mean.

--- Neural_network.py
+++ Neural_network.py
@@ -2,6 +2,27 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
+import pandas as pd
+from matplotlib import pyplot as plt
+import librosa 
+import librosa.display
+import IPython.display  as ipd 
+# Audio files path
+dataset_path = '/kaggle/input/deap-data/Deap-data/MEMD_audio'
+
+arousal = pd.read_csv('/kaggle/input/deap-dataset/Deap-data/arousal.csv')
+valence = pd.read_csv('/kaggle/input/deap-dataset/Deap-data/valence.csv')
+
+
+arousal['mean_arousal'] = arousal.iloc[:, 1:].mean(axis=1)
+valence['mean_valence'] = valence.iloc[:, 1:].mean(axis=1)
+
+arousal = arousal[['song_id', 'mean_arousal']]
+valence = valence[['song_id', 'mean_valence']]
+
+merged = pd.merge(arousal, valence, on='song_id')
+
+
 
 class Emotion_Classifier(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
